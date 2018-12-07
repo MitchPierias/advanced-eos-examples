285f8c3abec7162738a7a2110c856871b72747ed
Mitch Pierias <mitch@pierias.com>
2018-12-07 21:55:43 +1000

    Minor bug fixes
    - Compilation issue remains with multi-index tables

--- 03_Table-Indexes/timeline.cpp
+++ 03_Table-Indexes/timeline.cpp
@@ -10,10 +10,11 @@ class timeline : public eosio::contract {
 	public:
 
 		using contract::contract;
+		// Constructor
+		timeline(action_name self) : contract(self), tweets(_self, _self) {}
 		// @abi action
 		void post(const account_name username, uint64_t index, const string& msg_str) {
 			// Create and store tweet
-			tweet_table tweets(_self, _self);
 			tweets.emplace(username, [&](auto& tweet) {
 				tweet.id = index;
 				tweet.author = username;
@@ -23,14 +24,12 @@ class timeline : public eosio::contract {
 		// @abi action
 		void get(uint64_t index) {
 			// Get tweet at id
-			tweet_table tweets(_self, _self);
 			auto tweet = tweets.get(index);
 			print(tweet.msg);
 		}
 		// @abi action
 		void find(const account_name account) {
 			// Fetch tweets and display
-			tweet_table tweets(_self, _self);
 			auto author_index = tweets.get_index<N(byowner)>();
 			auto iter = author_index.lower_bound(account);
 			while (iter != author_index.end()) {
@@ -45,14 +44,15 @@ class timeline : public eosio::contract {
 			uint64_t		id;
 			account_name 	author;
 			string			msg;
-			bool			flagged = false
+			bool			flagged = false;
 
-			auto primary_key() const { return id; }
+			auto primary_key() const { return id; };
 			uint64_t get_author() const { return author; };
 			EOSLIB_SERIALIZE(Tweet, (id)(author)(msg)(flagged));
 		};
 
 		typedef multi_index<N(tweets), Tweet, indexed_by<N(byauthor), const_mem_fun<Tweet, uint64_t, &Tweet::get_author>>> tweet_table;
+		tweet_table tweets;
 };
 
-EOSIO_ABI(indexes, (post)(get)(find));
+EOSIO_ABI(timeline, (post)(get)(find));
